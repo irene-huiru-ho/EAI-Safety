d3baf30cab11c1ebb82c2a84a9b4e2d5a1b4ae33
irene-huiru-ho <hho24@wisc.edu>
2026-05-12 23:58:02 -0500

    update port num in server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const submissionsRouter = require('./routes/submissions');
 const researcherRouter = require('./routes/researcher');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
