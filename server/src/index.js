4f0f34026ba1cebce5344762c3934d4820776f5b
irene-huiru-ho <hho24@wisc.edu>
2026-05-13 00:13:40 -0500

    update port 0.0.0.0

--- server/src/index.js
+++ server/src/index.js
@@ -42,6 +42,6 @@ if (isProd) {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
